3b3fc106decdd055f924441ff714578c6c7e4479
Lucas Inocente <olucassanchez@gmail.com>
2020-02-03 19:52:50 -0300

    Remove wrong data

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAp8p3PiCf2oFgz3NP7JzTqgnwM657tKU",
+  apiKey: "",
   databaseURL: "https://simple-like-oss.firebaseio.com",
   projectId: "simple-like-oss",
   storageBucket: "simple-like-oss.appspot.com"
