eee45b6bb3e90a171e031783f1ebf0067e34f6ed
Lucas Inocente <olucassanchez@gmail.com>
2020-03-11 00:31:51 -0300

    Add infos, style and configuration to be shared

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import { DireflowComponent } from 'direflow-component';
 import App from './direflow-component/App';
 
-const direflowComponent = new DireflowComponent();
+const simpleLike = new DireflowComponent();
 
-direflowComponent.setProperties({
-  sampleList: [
-    'Create with React',
-    'Build as Web Component',
-    'Use it anywhere!',
-  ],
-});
-
-direflowComponent.render(App, 'simple-like');
+simpleLike.render(App, 'simple-like');
